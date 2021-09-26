40c976d1bd8d8069ba01a8e22fe309b00d8789f5
Darren Chen <44047795+darrenjundachen@users.noreply.github.com>
2021-09-27 07:05:19 +1000

    backup

--- training/2021_09_26_23_09/configs/constants.py
+++ training/2021_09_26_23_09/configs/constants.py
@@ -2,7 +2,7 @@ SEQ_LEN = 60
 EPOCHS = 20
 BATCH_SIZE = 64
 LEARNING_RATE = 0.001
-TARGET_MINS = 360  # Selected target mins, 1440 for 24 hours
+TARGET_MINS = 360  # Selected target mins, 360 for 6 hours
 TRAIN_TEST_SPLIT = 0.7  # Percentage for train
 PRICE_INC_RATE = 0.02
 
